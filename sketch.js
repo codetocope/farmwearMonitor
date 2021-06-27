6906dbb84cdd1959608e538fbfa8ca187b20446f
JaNieceCampbell <jmc31899@gmail.com>
2021-06-27 15:30:19 -0500

    responsiveness base work

--- sketch.js
+++ sketch.js
@@ -8,55 +8,34 @@ function preload() {
 }
 
 function setup() {
-    cnv = createCanvas(720, 454);
+
+    frameRate(12);
+    //cnv = createCanvas(720, 454);
+    let videoWidth = windowWidth/4;
+    cnv = createCanvas((windowWidth/3) * (4/3), windowWidth/3);
     video = createCapture(VIDEO);
     background(0);
-    video.size(360, 227);
+    //video.size(360, 227);
+    video.size(videoWidth * (4/3), videoWidth);
     video.hide();
 
     cnv.parent('cnvContainer');
-
-    screenshotBtn = select('#screenshotBtn');
-
-    screenshotBtn.elt.addEventListener('click', (event) => {
-        html2canvas(document.body).then(canvas => {
-            saveAs(canvas.toDataURL(), 'screenshot.jpg');
-        });
-    });
 
     video.elt.addEventListener('loadeddata', (event) => {
         if (data != null) {
             knnClassifier.load('plantHealthDataset2.json', function () {
                 console.log('dataset loaded');
-
+    
                 classify();
                 state = 'prediction';
             });
+        } else {
+            console.log('failed to load JSON data');
         }
-    });
+    }); 
 }
 
 function draw() {
     background(0);
     image(video, 0, 0, width, height);
-}
-
-function saveAs(uri, filename) {
-
-    var link = document.createElement('a');
-
-    if (typeof link.download === 'string') {
-
-        link.href = uri;
-        link.download = filename;
-
-        link.click();
-        
-        document.body.removeChild(link);
-
-    } else {
-
-        window.open(uri);
-
-    }
 }
